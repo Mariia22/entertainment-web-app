fcec4b44a866eb64b2d69f5218f8f38b7cd47e66
Mariia Ushakova <m.ushakova22@gmail.com>
2023-12-18 20:02:09 +0100

    style: add body styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,16 @@ import type { Config } from "tailwindcss"
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        colors: {
+            contrast: "#FC4747",
+            primary: "#10141E",
+            secondary: "#5A698F",
+            primaryPale: "#161D2F",
+            text: "#FFFFFF",
+        },
+        fontFamily: {
+            sans: ["Outfit Light", "sans-serif"],
+        },
         extend: {},
     },
     plugins: [],
