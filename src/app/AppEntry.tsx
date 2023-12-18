3e6c47a7ce0a44f45f5835fa5619193e23f33ba8
Mariia Ushakova <m.ushakova22@gmail.com>
2023-12-18 15:46:40 +0100

    feat: add tailwind library

--- src/app/AppEntry.tsx
+++ src/app/AppEntry.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <div>Hello!</div>
+        <h1 className="text-3xl font-bold underline">Hello!</h1>
     </React.StrictMode>
 )
