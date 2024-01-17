ebd250fe4470cdf3a48fceb46fb7fd4b59180ab0
Mariia Ushakova <m.ushakova22@gmail.com>
2024-01-17 16:29:17 +0100

    doc: add path

--- src/app/ui/baseLayoutUnauthorized.tsx
+++ src/app/ui/baseLayoutUnauthorized.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Outlet } from "react-router-dom"
 import LogoIcon from "../../app/assets/icons/logo.svg"
 
-export const BaseLayoutUnauthorized = (
+export const BaseLayoutUnauthorized: React.ReactNode = (
     <div className="flex flex-col items-center font-light font-outfit min-w-[375px] mt-20 overflow-hidden">
         <LogoIcon />
         <Outlet />
