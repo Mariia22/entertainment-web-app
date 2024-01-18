4b7e2f5f76378018b6d19460f758616f03747f8a
Mariia Ushakova <m.ushakova22@gmail.com>
2024-01-18 13:48:57 +0100

    fix: path to avatar

--- src/shared/ui/UserAvatar/UserAvatar.tsx
+++ src/shared/ui/UserAvatar/UserAvatar.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export const UserAvatar: React.FC = () => {
-    return (
-        <div className="w-6 h-6 bg-[url('./../../../public/image-avatar.png')] bg-cover bg-no-repeat bg-center cursor-pointer justify-self-end border border-solid border-text rounded-full xl:place-self-end xl:w-10 xl:h-10" />
-    )
+  return (
+    <div className="w-6 h-6 bg-[url('./assets/avatar.png')] bg-cover bg-no-repeat bg-center cursor-pointer justify-self-end border border-solid border-text rounded-full xl:place-self-end xl:w-10 xl:h-10" />
+  )
 }
