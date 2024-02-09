b8903f09619ca4eca4ad6d821fd7a4b40129f83a
Mariia Ushakova <m.ushakova22@gmail.com>
2024-02-09 18:27:28 +0100

    fix: max width for wide screen

--- src/app/ui/baseLayout.tsx
+++ src/app/ui/baseLayout.tsx
@@ -7,14 +7,14 @@ import { UserAvatar } from "../../shared/ui/UserAvatar/UserAvatar"
 import { PageWrapper } from "../../shared/ui/PageWrapper/PageWrapper"
 
 export const BaseLayout: React.ReactNode = (
-    <div className="flex flex-col font-light font-outfit min-w-[375px] w-screen overflow-hidden xl:flex-row">
-        <LayoutHeader>
-            <Logo />
-            <Navigation />
-            <UserAvatar />
-        </LayoutHeader>
-        <PageWrapper>
-            <Outlet />
-        </PageWrapper>
-    </div>
+  <div className="flex flex-col font-light font-outfit w-screen overflow-hidden xl:flex-row 2xl:justify-center">
+    <LayoutHeader>
+      <Logo />
+      <Navigation />
+      <UserAvatar />
+    </LayoutHeader>
+    <PageWrapper>
+      <Outlet />
+    </PageWrapper>
+  </div>
 )
--- src/widgets/TrendingSection/TrendingSection.tsx
+++ src/widgets/TrendingSection/TrendingSection.tsx
@@ -6,31 +6,31 @@ import { selectTrends } from "../../entities/entertainment/model/slice"
 import { CardDescription } from "../CardDescription/CardDescription"
 
 export const TrendingSection: React.FC = () => {
-    const trendMovies = useAppSelector(selectTrends)
+  const trendMovies = useAppSelector(selectTrends)
 
-    if (trendMovies.length === 0) {
-        return <div className="flex pt-4">{errorMessage.emptyTrending}</div>
-    }
+  if (trendMovies.length === 0) {
+    return <div className="flex pt-4">{errorMessage.emptyTrending}</div>
+  }
 
-    return (
-        <div className="flex w-screen pt-4 gap-4 overflow-x-scroll md:gap-x-10">
-            {trendMovies.map((movie) => (
-                <Card
-                    key={movie.title}
-                    title={movie.title}
-                    url={movie.thumbnail.trending.small}
-                    isBookmarked={movie.isBookmarked}
-                    isTrendingSection={true}
-                >
-                    <CardDescription
-                        title={movie.title}
-                        year={movie.year}
-                        genre={movie.category}
-                        rating={movie.rating}
-                        isTrending={true}
-                    />
-                </Card>
-            ))}
-        </div>
-    )
+  return (
+    <div className="flex w-screen pt-4 gap-4 overflow-x-scroll max-w-[1240px] md:gap-x-10">
+      {trendMovies.map((movie) => (
+        <Card
+          key={movie.title}
+          title={movie.title}
+          url={movie.thumbnail.trending.small}
+          isBookmarked={movie.isBookmarked}
+          isTrendingSection={true}
+        >
+          <CardDescription
+            title={movie.title}
+            year={movie.year}
+            genre={movie.category}
+            rating={movie.rating}
+            isTrending={true}
+          />
+        </Card>
+      ))}
+    </div>
+  )
 }
